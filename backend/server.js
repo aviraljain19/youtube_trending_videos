f2cfff717ab56f753cc7af415473e3cb3d334701
Aviral Jain <aviraljainltr@gmail.com>
2024-12-18 15:59:04 +0530

    Setting up server and MongoDB
    Signed-off-by: Aviral Jain <aviraljainltr@gmail.com>

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
