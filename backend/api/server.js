e71925b39606d1d0b843c7007462b0b219346396
Aviral Jain <aviraljainltr@gmail.com>
2024-12-20 03:35:01 +0530

    Some changes
    Signed-off-by: Aviral Jain <aviraljainltr@gmail.com>

--- backend/api/server.js
+++ backend/api/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const videoRoutes = require("./routes/videoRoutes");
+const videoRoutes = require("../routes/videoRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
